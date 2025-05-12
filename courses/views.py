42e6afa78a40653df65403679170853ed20c0276
Ferhat Çelik <ferhatsteel34@gmail.com>
2025-05-12 16:25:11 +0300

    Lesson 2.6 finished

--- courses/views.py
+++ courses/views.py
@@ -4,16 +4,19 @@ from django.shortcuts import render
 def kurslar(request):
     return HttpResponse("kurs listesi")
 
-def details(request):
-    return HttpResponse("kurs detay sayfası")
+def details(request, kurs_adi):
+    return HttpResponse(f"{kurs_adi} detay sayfası")
 
-def getCoursesByCategory(request, category):
+def getCoursesByCategory(request, category_name):
     text = ""
 
-    if(category == "programlama"):
+    if(category_name == "programlama"):
         text = "programlama kategorisine ait kurslar"
-    elif (category == "web-gelistirme"):
+    elif (category_name == "web-gelistirme"):
         text = "web-gelistirme kategorisine ait kurslar"
     else:
         text = "yanlış kategori seçimi"
     return HttpResponse(text)
+
+def getCoursesByCategoryId(request, category_id):
+    return HttpResponse(category_id)
